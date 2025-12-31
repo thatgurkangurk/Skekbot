c4b872edc896fd67f076d3a63042390b2164e7d0
Skekdog <47750815+Skekdog@users.noreply.github.com>
2025-12-31 18:37:27 +0000

    add missing DEV_SERVER_ID env var

--- Source/Utility/env.ts
+++ Source/Utility/env.ts
@@ -2,4 +2,5 @@ export const env = {
 	BOT_TOKEN: process.env["BOT_ID"],
 	PORT: Number.parseInt(process.env["PORT"] ?? "443"),
 	WEB_PASSWORD: process.env["WEB_PASSWORD"],
+	DEV_SERVER_ID: process.env["DEV_SERVER_ID"], // note: this is a string, due to discord.js
 };
