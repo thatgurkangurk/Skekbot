35c9c560ae2670ffb29b19cbab58d3689a64714f
Gurkan <hello@gurkz.me>
2025-12-31 17:59:06 +0100

    authenticate requests to /send-message

--- Source/Web/Page/skekbot.js
+++ Source/Web/Page/skekbot.js
@@ -1,11 +1,32 @@
 /* eslint-disable no-undef */
+// @ts-check
 
-const messageArea = document.getElementById("message-box");
-const sendButton = document.getElementById("message-button");
-const channelInput = document.getElementById("channel-input");
-const replyInput = document.getElementById("reply-input");
+const messageArea = /** @type {HTMLTextAreaElement | null} */ (
+	document.getElementById("message-box")
+);
 
-sendButton.onclick = async () => {
+const sendButton = /** @type {HTMLInputElement | null} */ (
+	document.getElementById("message-button")
+);
+
+const channelInput = /** @type {HTMLInputElement | null} */ (
+	document.getElementById("channel-input")
+);
+
+const replyInput = /** @type {HTMLInputElement | null} */ (
+	document.getElementById("reply-input")
+);
+
+const webTokenInput = /** @type {HTMLInputElement | null} */ (
+	document.getElementById("web-token")
+);
+
+sendButton?.addEventListener("click", async () => {
+	if (!channelInput || !messageArea || !replyInput || !webTokenInput) {
+		console.warn("some input is missing!");
+		return;
+	}
+
 	const response = await fetch("send-message", {
 		body: JSON.stringify({
 			channelId: channelInput.value,
@@ -14,10 +35,11 @@ sendButton.onclick = async () => {
 		}),
 		headers: {
 			"Content-Type": "application/json",
+			"Authorization": `Bearer ${webTokenInput.value}`
 		},
 		method: "POST",
 	});
 
 	const output = await response.text();
 	alert(output);
-};
+});
